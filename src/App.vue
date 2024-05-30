6a71f2f40fd11b6dff412695ae324972697c6051
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-30 15:49:00 +0200

    M2 incompleta

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
-import axios from 'axios'
 
 
 
@@ -15,32 +14,14 @@ export default {
   data() {
     return {
 
-      movies: [],
+      
 
     }
   },
 
   methods: {
 
-    getMovies() {
-      const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/movie/popular',
-        params: { language: 'en-US', page: '1' },
-        headers: { accept: 'application/json', Authorization: 'Bearer 109522a1ecd9c1b26ccd0c3129e6a34d' }
-      };
-
-      axios
-        .request(options)
-        .then(response => {
-          console.log(response.data);
-          this.movies=response.data.results;
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-
+    
   },
 
   mounted() {
@@ -54,7 +35,7 @@ export default {
 <template>
 
   <AppHeader />
-  <AppMain :movies="movies" />
+  <AppMain />
 
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,30 +1,56 @@
 <script>
 
 
+import axios from 'axios'
 
 
 export default {
 
-    name:'AppMain',
-    props: {
-        movies: {Array},
-        required: true
+    name: 'AppMain',
+
+
+    data() {
+        return {
+
+            movies: [],
+            flagLanguage: {
+                "en" : require("../assets/Flag_of_the_United_Kingdom_(1-2).svg.png"),
+                "it" : require("../assets/Flag_of_Italy.svg.png")         
+            }
+
+
+        }
+
     },
-  
-  data() {
-    return {
-        
+    methods: {
 
-    }
+        getMovies() {
+            const options = {
+                method: 'GET',
+                url: 'https://api.themoviedb.org/3/movie/popular',
+                params: { language: 'en-US', page: '1' },
+                headers: { accept: 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDk1MjJhMWVjZDljMWIyNmNjZDBjMzEyOWU2YTM0ZCIsInN1YiI6IjY2NTczMzg1ZWYwNWQ5MDUwNTU5OTc5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5Ms9JOIUtSXGUmtaxu-i83znnXnCT6ekKKp90y5qWeM' }
+            };
 
-  },
-  methods: {
+            axios
+                .request(options)
+                .then(response => {
+                    console.log(response.data.results);
+                    this.movies = response.data.results;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
 
-  },
 
-  mounted() {
+    },
 
-  },
+    mounted() {
+
+        this.getMovies()
+
+    },
 
 }
 
@@ -32,31 +58,43 @@ export default {
 
 <template>
 
- <div class="container-fluid st_container bg-black d-flex align-items-center">
-    <div class="row d-flex justify-content-between col-12 m-0">
-                <div class="col-2 st_boolflix">
-                    BOOLFLIX
-                </div>
-                <div class="col-2">
-                    <input type="text">
-                </div>
+    <div class="container-fluid st_container bg-black d-flex align-items-center">
+        <div class="row d-flex justify-content-between col-12 m-0">
+            <div class="col-2 st_boolflix">
+                BOOLFLIX
+            </div>
+            <div class="col-2">
+                <input type="text">
+            </div>
+        </div>
+
     </div>
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-10">
-                <ul>
-                    <li></li>
+
+            <div class="offset-1 col-10 bg-info p-0 d-flex
+            ">
+                <ul class="d-flex flex-wrap">
+                    <li class="m-2" v-for="element in movies">
+                        <div>{{ element.title }}</div>
+                        <div>{{ element.original_title }}</div>
+                        <div>
+                            <img :src="flagLanguage[element.original_language]" alt=""
+                            v-if="flagLanguage[element.original_language]">
+                            <span v-else>{{ element.original_language }}</span>  
+                        </div>
+                        <div>{{ element.vote_average }}</div>
+                    </li>
                 </ul>
             </div>
         </div>
     </div>
- </div>
-  
+
 
 </template>
 
-<style>
+<style scoped>
 .st_boolflix {
     color: red;
 }
@@ -65,5 +103,10 @@ export default {
 
     height: 5rem;
 
+}
+
+.st_container2 {
+    margin-left: 20rem;
+    margin-right: 20rem
 }
 </style>
